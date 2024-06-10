1b2e22400ad176115433244173ef8ef9ade67aa2
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-11 00:45:47 +0400

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template, request
+from backend import find_word
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    search_word = request.form['search_word']
+    results = find_word(search_word)
+    return render_template('search_results.html', search_word=search_word, results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
